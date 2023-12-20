d7eca18934954f47fbe20ef571f730126e73ab4a
Pedro José R. R <pedroreyes@profesorado.cei.es>
2023-12-20 14:27:20 +0100

    Volcado de archivos

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - Declaramos una variable vacía que contendrá el número aleatorio.
 - Declaramos una variable que contenga el botón del HTML que va a comprobar el número.
 - Declaramos una variable que contenga el botón del HTML que va a limpiar el localStorage.
+- Declaramos una variable que contenga el div del HTML que tendrá el texto de resultado.
+- Declaramos una variable que contenga el div del HTML que tendrá el texto de estadísticas
 - Declaramos una variable que contenga la obtención del valor de "exitosos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave.
 - Declaramos una variable que contenga la obtención del valor de "fallidos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave.
 
@@ -13,9 +15,42 @@
 ### Función generadora del número
 
 - Declaramos una función que generará el número aleatorio.
--- Le añade como valor a la variable que va a contener dicho número un random como número entero.
+    - Le añade como valor a la variable que va a contener dicho número un random como número entero.
 
 ### Función verificadora de número
 
 - Declaramos una función que comprobará el número que agreguemos.
-    - Declaramos
+    - Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
+    - Ese valor lo transformamos en un número entero (parseInt).
+    - Si el número del usuario es estrictamente igual que el número almacenado en la variable que contiene el número aleatorio:
+        - Aumentamos el valor de la variable que contiene el número de éxitos.
+        - Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
+        - Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "¡Muy bien!".
+        - Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+        - Llamamos a la función generadora de números para generar un nuevo número aleatorio.
+    - Si no:
+        - Aumentamos el valor de la variable que contiene el número de fallidos.
+        - Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de fallidos.
+        - Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "Fallaste".
+        - Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+
+### Función limpiadora de datos
+
+- Declaramos una función que se encargará de limpiar los datos del localStorage
+    - Usamos el método de localStorage para limpiar los datos.
+    - Utilizamos "location.reload()" para reiniciar la página.
+
+## Listeners
+
+### Botón comprobar
+
+- Creamos un addEventListener para que al hacer click en el botón de comprobar se active la función verificadora de número.
+
+### Botón limpiar
+
+- Creamos un addEventListener para que al hacer click en el botón de limpiar se active la función limpiadora de datos.
+
+## Códigos de inicio
+
+> [!NOTE]
+> Useful information that users should know, even when skimming content.
--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 let numeroSecreto; // Declaramos una variable que contenga la "llamada" a una función que generará el número aleatorio
 let botonComprobar = document.querySelector('#comprobar') // Declaramos una variable que contenga el botón del HTML que va a comprobar el número
 let borrarDatos = document.querySelector('.borrarDatos') // Declaramos una variable que contenga el botón del HTML que va a limpiar el localStorage
+let textoResultado = document.querySelector('#resultado') // Declaramos una variable que contenga el div del HTML que tendrá el texto de resultado.
+let textoEstadisticas = document.querySelector('#estadisticas') // Declaramos una variable que contenga el div del HTML que tendrá el texto de estadísticas
 let intentosExitosos = localStorage.getItem('exitosos') || 0; // Declaramos una variable que contenga la obtención del valor de "exitosos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave
 let intentosFallidos = localStorage.getItem('fallidos') || 0; // Declaramos una variable que contenga la obtención del valor de "fallidos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave
 
 function generarNumeroAleatorio() { // Declaramos una función que generará el número aleatorio
     numeroSecreto = Math.floor(Math.random() * 10); // Añadimos un 
+    console.info(numeroSecreto)
 }
 
 botonComprobar.addEventListener('click', verificarNumero)
@@ -15,33 +18,17 @@ function verificarNumero() {
     numeroUsuario = parseInt(numeroUsuario);
 
     if (numeroUsuario === numeroSecreto) {
-        actualizarEstadisticas(true);
-        mostrarResultado("¡Correcto! El número era " + numeroSecreto);
-        generarNumeroAleatorio();
-    } else {
-        actualizarEstadisticas(false);
-        mostrarResultado("Incorrecto, intenta de nuevo.");
-    }
-}
-
-function actualizarEstadisticas(esExitoso) {
-    if (esExitoso) {
         intentosExitosos++;
         localStorage.setItem('exitosos', intentosExitosos);
+        textoResultado.innerHTML = "<div class='correcto'><span style='color:green'>✔</span> ¡Correcto! El número era <b>" + numeroSecreto + "</b></div>";
+        textoEstadisticas.innerHTML = `Exitosos: ${intentosExitosos}, Fallidos: ${intentosFallidos}`;
+        generarNumeroAleatorio();
     } else {
         intentosFallidos++;
-        localStorage.setItem('fallidos', intentosFallidos);
+        localStorage.setItem('fallidos', intentosExitosos);
+        textoResultado.innerHTML = "<div class='incorrecto'><span style='color:red'>✘</span> Incorrecto, intenta de nuevo.</div>";
+        textoEstadisticas.innerHTML = `Exitosos: ${intentosExitosos}, Fallidos: ${intentosFallidos}`;
     }
-    mostrarEstadisticas();
-}
-
-function mostrarResultado(mensaje) {
-    document.getElementById('resultado').textContent = mensaje;
-}
-
-function mostrarEstadisticas() {
-    let mensaje = `Exitosos: ${intentosExitosos}, Fallidos: ${intentosFallidos}`;
-    document.getElementById('estadisticas').textContent = mensaje;
 }
 
 borrarDatos.addEventListener('click', borrarDatosLocales)
@@ -51,5 +38,6 @@ function borrarDatosLocales() {
     location.reload();
 }
 
+textoEstadisticas.innerHTML = `Exitosos: ${intentosExitosos}, Fallidos: ${intentosFallidos}`;
+
 generarNumeroAleatorio()
-mostrarEstadisticas();
